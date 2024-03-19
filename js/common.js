1b07ce0e72e3091cc87649fa5153cbd20f4ab1a5
junb119 <emfowkd@gmail.com>
2024-03-19 15:42:40 +0900

    header footer module

--- js/common.js
+++ js/common.js
@@ -3,6 +3,10 @@
 
 //header 이벤트들
 $(function(){
+  $('#header').load('./header.html')
+  $('#footer').load('./footer.html')
+
+
     let header = $('#header'),
         headerLogo = header.find('.logo img'),
         mainMenu = $('.gnb_wrap .gnb>li>a'),
@@ -15,7 +19,7 @@ $(function(){
         // header scroll,mouseenter 이벤트 효과 함수 생성
         function headerEvent(){
           header.addClass('fixed');
-          mainMenu.addClass('on');
+          mainMenu.addClass('on');  
           langMenu.addClass('on');
           headerLogo.attr('src','images/main_logo.png');
         }
