e4630be49226a94c5403da69fdfe643df4c7f9ff
songrim <150899105+aengkrrrrr@users.noreply.github.com>
2024-03-20 23:22:45 +0900

    popup 생성

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
+// 팝업 - 송림   
+const popup = document.querySelector('.popup');
+const input = popup.querySelector('input');
+const closeBtn =popup.querySelector('button');
+
+
+closeBtn.addEventListener('click',()=>{
+  // console.log(in5660put.checked);
+  if(input.checked){
+    //쿠키생성
+    setCookie('Portfolio','MiraeAsset', 1);
+  }else{
+    //쿠키삭제
+    delCookie('Portfolio');
+  }
+  popup.classList.add('hide');
+});
+function setCookie(name, val, day){
+    let date = new Date();
+    date.setDate(date.getDate()+day);
+    document.cookie = `${name}=${val};Expires=${date}`;
+  }
+  function delCookie(name){
+    let date = new Date();
+    date.setDate(date.getDate()-1);
+    document.cookie = `${name}='';Expires=${date}`;
+  }
+
+  function checkCookie(name){
+    let cookieArr = document.cookie.split(';');
+    let visited = false;
+
+    for(let cookie of cookieArr){
+      if(cookie.indexOf(name) > -1) {
+        visited = true;
+      }
+    }
+    if(visited) {
+      popup.classList.add('hide');
+    } else {
+      popup.classList.remove('hide');
+    }
+  }
+  checkCookie('MiraeAsset');
+// --팝업 - 송림 
 
 // 헤더 - 송림   
 
