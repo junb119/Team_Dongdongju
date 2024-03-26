c064d0b2d935754c7fad4fd3e88788c8771e683f
sjpark071~ <mintgreent@gmail.com>
2024-03-26 18:24:15 +0900

    csr.css기능수정

--- js/csr_info.js
+++ js/csr_info.js
@@ -1,3 +1,25 @@
-$( function() {
-  $( "#accordion" ).accordion();
-} );
+const panel = document.querySelectorAll('.panel'),
+  subtitle= document.querySelector('.subtitle'),
+  panelContents = document.querySelectorAll('.panel-contents');
+
+for (let p of panel) {
+  p.addEventListener('click', (event)=>{
+    hideAll();
+    p.classList.add('active');
+    subtitle.style.display= "none";
+    panelContents.style.top = "124px";
+  });
+}
+  
+// for(p2 of panel){
+// p2.addEventListener('click',(event)=>{
+// var accordion = event.currentTarget;
+// var accordions = accordion.parentNode;
+// accordions.insertBefore(accordion, accordions.firstChild);
+// })
+// }
+function hideAll() {
+  for (let p of panel) {
+    p.classList.remove('active');
+  }
+}
