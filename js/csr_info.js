b953aa615b4a9b6631cd6a985075d637b6942c19
sjpark07 <mintgreent@gmail.com>
2024-03-27 00:00:32 +0900

    CSR INFO 기능추가

--- js/csr_info.js
+++ js/csr_info.js
@@ -1,12 +1,16 @@
 const panel = document.querySelectorAll('.panel'),
   subtitle= document.querySelector('.subtitle'),
   panelContents = document.querySelectorAll('.panel-contents');
-
+ cta = document.getElementById('cta');
+ panelactive = document.querySelectorAll('.panel.active');
+ panelsibling = panelactive.nextElementSibling ;
 for (let p of panel) {
-  p.addEventListener('click', (event)=>{
+  p.addEventListener('click', ()=>{
     hideAll();
     p.classList.add('active');
     subtitle.style.display= "none";
+    cta.style.display= "none";
+    panelsibling.style.height = "10vh";
     panelContents.style.top = "124px";
   });
 }
