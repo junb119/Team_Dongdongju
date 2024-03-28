8b99c42426e389f4dc389dcf91382c87fc43bbca
songrim <srimm3399@naver.com>
2024-03-28 18:13:51 +0900

    html기능추가, css기능추가 js기능추가

--- js/common.js
+++ js/common.js
@@ -22,10 +22,12 @@ $(function () {
       langMenu.addClass("on");
       headerLogo.attr("src", "images/main/main_header/main_logo.png");
     }
-    // header scroll,mouseenter 이벤트 효과 함수 생성
-
+    // --header scroll,mouseenter 이벤트 효과 함수 생성
+    
+    //header scroll 이벤트
     $(window).on("scroll", function () {
       var st = $(this).scrollTop();
+   
       if (st > 0) {
         headerEvent();
       } else {
@@ -34,37 +36,50 @@ $(function () {
         langMenu.removeClass("on");
         headerLogo.attr("src", "images/main/main_header/main_logo_white.png");
       }
+    
     });
-
+    
     subMenu.each(function () {
       if (subMenuHeight < $(this).height()) {
         subMenuHeight = $(this).height();
       }
     });
+    //--header scroll 이벤트
+    
+    //header mouseenter,leave 이벤트
     header
-      .on("mouseenter", function () {
-        header
-          .stop()
-          .animate({ height: headerHeight + subMenuHeight + 75 + "px" });
-        headerEvent();
-      })
-      .on("mouseleave", function () {
-        header.stop().animate({ height: headerHeight + "px" });
-      });
+    .on("mouseenter", function () {
+      header.stop().animate({ height: headerHeight + subMenuHeight + 75 + "px" });
+      headerEvent();
+
+    })
+    .on("mouseleave", function () {
+      header.stop().animate({ height: headerHeight + "px" });
+      
+      // if($(window).scrollTop() = 0){
+        
+      //   header.removeClass("fixed");
+      //   mainMenu.removeClass("on");
+      //   langMenu.removeClass("on");
+      //   headerLogo.attr("src", "images/main/main_header/main_logo_white.png");
+      // }
+    });
+    // --header mouseenter,leave 이벤트
+    
     // scroll시 헤더 업 다운 이벤트
     var lastScrollTop = 0;
     $(window).scroll(function () {
       var st = $(this).scrollTop();
       if (st > lastScrollTop) {
         // 스크롤 다운 시 실행할 코드
-        header.slideDown(); // header를 위로 사라지게 함
+        header.slideUp(); // header를 위로 사라지게 함
       } else {
         // 스크롤 업 시 실행할 코드
-        header.slideUp(); // header를 아래로 나타나게 함
+        header.slideDown(); // header를 아래로 나타나게 함
       }
       lastScrollTop = st;
     });
-    // scroll시 헤더 업 다운 이벤트
+    // --scroll시 헤더 업 다운 이벤트
   }
 
   //header 이벤트들
