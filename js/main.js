a43b126e7b4946fc6c764e46d28bee3770f9cc23
junb119 <emfowkd@gmail.com>
2024-03-29 18:22:36 +0900

    기능추가 스무스 스크롤 애니메이션

--- js/main.js
+++ js/main.js
@@ -293,3 +293,55 @@ mainNews.mouseenter(function () {
 // 푸터 - 선진
 // --푸터 - 선진
 // award 슬라이드 이벤트
+
+
+
+
+class Scrooth {
+  constructor({element = window, strength=10, acceleration = 1.2,deceleration = 0.975}={}) {
+    this.element = element;
+    this.distance = strength;
+    this.acceleration = acceleration;
+    this.deceleration = deceleration;
+    this.running = false;
+
+    this.element.addEventListener('wheel', this.scrollHandler.bind(this), {passive: false});
+    this.element.addEventListener('mousewheel', this.scrollHandler.bind(this), {passive: false});
+    this.scroll = this.scroll.bind(this);
+  }
+
+  scrollHandler(e) {
+    e.preventDefault();
+
+    if (!this.running) {
+      this.top = this.element.pageYOffset || this.element.scrollTop || 0;
+      this.running = true;
+      this.currentDistance = e.deltaY > 0 ? 0.1 : -0.1;
+      this.isDistanceAsc = true;
+      this.scroll();
+    } else {
+      this.isDistanceAsc = false;
+      this.currentDistance = e.deltaY > 0 ? this.distance : -this.distance;
+    }
+  }
+
+  scroll() {
+    if (this.running) {
+      this.currentDistance *= this.isDistanceAsc === true ? this.acceleration : this.deceleration;
+      Math.abs(this.currentDistance) < 0.1 && this.isDistanceAsc === false ? this.running = false : 1;
+      Math.abs(this.currentDistance) >= Math.abs(this.distance) ? this.isDistanceAsc = false : 1;
+
+      this.top += this.currentDistance;
+      this.element.scrollTo(0, this.top);
+      
+      requestAnimationFrame(this.scroll);
+    }
+  }
+}
+
+// const scroll = new Scrooth({
+//   element: window,
+//   strength: 40,
+//   acceleration: 1.1,
+//   deceleration: 0.975,
+// });
