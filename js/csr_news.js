c64b8927fa28c71b1d078e6d6eede645c470d685
sjpark07 <mintgreent@gmail.com>
2024-03-31 03:58:49 +0900

    기능추가 cse newslist json데이터추가

--- js/csr_news.js
+++ js/csr_news.js
@@ -1,34 +1,50 @@
-const perPage = 10;
+const perPage = 8;
 const rows = document.querySelectorAll('.article');
-const rowsCount = rows.length; //100
-const pageCount = Math.ceil(rowsCount/perPage); //10
+const rowsCount = rows.length; //40
+const pageCount = Math.ceil(rowsCount/perPage); //5
 const numbers = document.querySelector('#numbers');
 const prevBtn = document.querySelector('.pagination .fa-arrow-left');
 const nextBtn = document.querySelector('.pagination .fa-arrow-right');
 
 
 //json 데이터 연결//
-
-const maxPageNum = 5;
-let pageGroupIdx = 0; //현재 페이지 그룹의 번호
-let targetUl = $('.product');
+let targetUl = $('.container');
 let listHtml = '';
 
-fetch('https://dummyjson.com/products/')
+fetch('./data/csr_news.json')
 .then(res => res.json())
 .then(result => makeList(result));
 
 function makeList(result){
   console.log(result);
-  result.products.forEach((item, idx)=>{  
-    listHtml+=`<li><a href="product.html?id=${idx +1}">item ${idx +1}</a></li>`;
+  let list = $(result.products);
+  list.each(function(idx, item){  
+    listHtml+=`<div class="article">
+    <a href="csr_news_enter.html?id=${idx +1}">
+      <div class="article_date">
+        <div class="date"> ${list[idx].date}</div>
+        <div class="source">${list[idx].source}</div>
+      </div>
+      <div class="article_title">${list[idx].title}</div>
+      <div class="article_contents">${list[idx].contents}미래에셋박현주재단은 아동복지시설의 운영을 지원하는 '2024년 미래에셋 희망나눔 공모사업 1차'를 시작했다..</div>
+    </a>
+  </div>`;
   });
   targetUl.html(listHtml);
+
+//말줄임표
+$(document).ready(function() {
+  $(".article_contents").each(function() {
+      var content = $(this).text();
+      if (content.length > 50) {
+          $(this).text(content.substring(0, 50) + '...');
+      }
+  });
+});
 }
 
-
-
-
+const maxPageNum = 5;
+let pageGroupIdx = 0; //현재 페이지 그룹의 번호
 
 //페이지네이션 생성하기
 for(let i = 1;i<=pageCount;i++){
@@ -60,9 +76,9 @@ function displayRow(idx){
     nw.style.display = '';
   }
   for(let nb of numberBtn){
-    nb.classList.remove('active');
+    nb.removeClass('active');
   }
-  numberBtn[idx].classList.add('active');  
+  numberBtn[idx].addClass('active');  
 }
 displayRow(0);
 
