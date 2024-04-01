8f3a73c3ec1f2c811ee34a197ea728b2dc3b05be
junb119 <emfowkd@gmail.com>
2024-04-01 18:26:51 +0900

    버그수정 header 높이 수정

--- js/common.js
+++ js/common.js
@@ -3,69 +3,70 @@
 
 //header 이벤트들
 $(function () {
-  $("#header").load("./header.html", setHeader);
-  $("#footer").load("./footer.html", setFooter);
+  $('#header').load('./header.html', setHeader);
+  $('#footer').load('./footer.html', setFooter);
 
   function setHeader() {
-    let header = $("#header"),
-      headerLogo = header.find(".logo img"),
-      mainMenu = $(".gnb_wrap .gnb>li>a"),
-      subMenu = $(".gnb_wrap .depth02"),
-      subMenuHeight = 0,
+    let header = $('#header'),
+      headerLogo = header.find('.logo img'),
+      mainMenu = $('.gnb_wrap .gnb>li>a'),
+      subMenu = $('.gnb_wrap .depth02'),
+      subMenuHeight = $(subMenu).height(),
       headerHeight = header.height(),
       mobileBtn = $('.m_menu_btn'),
       mobileToggle = $('.m_menu_btn span'),
       mobileMenu = $('.m_gnb_wrap .m_gnb > li'),
-      langMenu = $(".lang_wrap a");
+      langMenu = $('.lang_wrap a');
 
     // header scroll,mouseenter 이벤트 효과 함수 생성
+    subMenu
+    console.log('submenu', $(subMenu).height());
+    console.log('submenu', subMenu);
     function headerEvent() {
-      header.addClass("fixed");
-      mainMenu.addClass("on");
-      langMenu.addClass("on");
-      mobileToggle.addClass("on");
-      headerLogo.attr("src", "images/main/main_header/main_logo.png");
+      header.addClass('fixed');
+      mainMenu.addClass('on');
+      langMenu.addClass('on');
+      mobileToggle.addClass('on');
+      headerLogo.attr('src', 'images/main/main_header/main_logo.png');
     }
     // --header scroll,mouseenter 이벤트 효과 함수 생성
-    
 
     //header scroll 이벤트
-    $(window).on("scroll", function () {
+    $(window).on('scroll', function () {
       var st = $(this).scrollTop();
-   
+
       if (st > 0) {
         headerEvent();
       } else {
-        header.removeClass("fixed");
-        mainMenu.removeClass("on");
-        langMenu.removeClass("on");
-        mobileToggle.removeClass("on");
-        headerLogo.attr("src", "images/main/main_header/main_logo_white.png");
+        header.removeClass('fixed');
+        mainMenu.removeClass('on');
+        langMenu.removeClass('on');
+        mobileToggle.removeClass('on');
+        headerLogo.attr('src', 'images/main/main_header/main_logo_white.png');
       }
-    
     });
-    
+
     //--header scroll 이벤트
-    
+
     //header mouseenter,leave 이벤트
-    subMenu.each(function () {
-      if (subMenuHeight < $(this).height()) {
-        subMenuHeight = $(this).height();
-      }
-    });
-    
+    // subMenu.each(function () {
+    //   if (subMenuHeight < $(this).height()) {
+    //     subMenuHeight = $(this).height();
+    //   }
+    // });
+
     header
-    .on("mouseenter", function () {
-      if($(window).width() > 500) {
-        header.stop().animate({ height: headerHeight + subMenuHeight + 75 + "px" });
-        headerEvent();
-      }
-    })
-    .on("mouseleave", function () {
-      header.stop().animate({ height: headerHeight + "px" });
-    });
+      .on('mouseenter', function () {
+        if ($(window).width() > 500) {
+          header.stop().animate({ height: headerHeight + subMenuHeight + 75 + 'px' });
+          headerEvent();
+        }
+      })
+      .on('mouseleave', function () {
+        header.stop().animate({ height: headerHeight + 'px' });
+      });
     // --header mouseenter,leave 이벤트
-    
+
     // scroll시 헤더 업 다운 이벤트
     var lastScrollTop = 0;
     $(window).scroll(function () {
@@ -81,27 +82,21 @@ $(function () {
     });
     // --scroll시 헤더 업 다운 이벤트
 
-
-    
     //모바일 메뉴 이벤트
-    mobileBtn.click(function(){
+    mobileBtn.click(function () {
       $(this).toggleClass('open');
       $('.m_menu_wrap').toggleClass('open');
-    })
-    
-    mobileMenu.click(function(){
-        $(this).find('ul').slideToggle();
-        $(this).siblings().find('ul').slideUp();
-        $('.m_gnb em').toggleClass('open');
-        $(this).siblings().find('em').removeClass('open');
-    })
-  
+    });
+
+    mobileMenu.click(function () {
+      $(this).find('ul').slideToggle();
+      $(this).siblings().find('ul').slideUp();
+      $('.m_gnb em').toggleClass('open');
+      $(this).siblings().find('em').removeClass('open');
+    });
+
     //--모바일 메뉴 이벤트
   }
-  
-
-
-
 
   //------------------------------------header 이벤트들
 
@@ -111,11 +106,11 @@ $(function () {
     // -----//footer 이벤트들
 
     // ----backToTop
-    const btt = $(".backToTop");
+    const btt = $('.backToTop');
     const $window = $(window);
     $window.scroll(() => {
       let scrollAmt = $window.scrollTop();
-      scrollAmt > 1000 ? btt.addClass("active") : btt.removeClass("active");
+      scrollAmt > 1000 ? btt.addClass('active') : btt.removeClass('active');
     });
 
     btt.click((e) => {
@@ -125,7 +120,7 @@ $(function () {
       window.scrollTo({
         left: 0,
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     });
     // ----//backToTop
