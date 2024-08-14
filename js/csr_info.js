84d8b4e013a62990308d688c4cea684d01535904
jun <emfowkd@gmail.com>
2024-08-14 19:51:58 +0900

    image pass fix

--- js/csr_info.js
+++ js/csr_info.js
@@ -4,14 +4,20 @@ setHeaderFooter(() => {
 
 const panel = document.querySelectorAll(".panel"),
   subtitle = document.querySelector(".subtitle"),
-  panelContents = document.querySelectorAll(".panel-contents");
-cta = document.getElementById("cta");
-panelactive = document.querySelectorAll(".panel.active");
-panelsibling = panelactive.nextElementSibling;
+  cta = document.getElementById("cta");
+let panelContents = document.querySelectorAll(".panel-contents");
+
 for (let p of panel) {
   p.addEventListener("click", () => {
+    // let panelactive = document.querySelectorAll(".panel.active")[0];
+    // panelsibling = panelactive.nextElementSibling;
+    let panelsibling = p.sil;
+    console.log(p);
+    console.log(panelsibling);
+
     hideAll();
     p.classList.add("active");
+
     subtitle.style.display = "none";
     cta.style.display = "none";
     panelsibling.style.height = "10vh";
